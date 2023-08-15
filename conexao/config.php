5dd556bd63c58c3c2e1ccf68745cb7fca508f3f3
Gabriel Malheiros <gebrielmalheiros1515@gmail.com>
2023-08-15 17:10:19 -0400

    db.class

---
+++ conexao/config.php
@@ -0,0 +1,7 @@
+<?php
+define( 'DB_HOST', 'localhost' ); // set database host
+define( 'DB_USER', 'root' ); // set database user
+define( 'DB_PASS', '' ); // set database password
+define( 'DB_NAME', 'gabriel' ); // set database name
+define( 'SEND_ERRORS_TO', 'teste222222@gmail.com' ); //set email notification email address
+define( 'DISPLAY_DEBUG', true ); //display db errors?
